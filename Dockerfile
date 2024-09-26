506f924c6aceb27033644bd9b0c761eeb3fc5666
sam <samueljohnsiby@gmail.com>
2024-09-26 14:41:20 +0530

    firebase deployment

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
-### STAGE 1: Build ###
-FROM node:latest AS build
+FROM node:18-alpine AS builder
+
 WORKDIR /usr/src/app
-COPY package.json package-lock.json ./
-RUN npm install
+COPY package*.json ./
+
+
+RUN npm install 
+
 COPY . .
 RUN npm run build
-### STAGE 2: Run ###
-FROM nginx:1.17.1-alpine
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY --from=build /usr/src/app/dist/acharya-frontend /usr/share/nginx/html
+
+
+CMD npm start
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { MarkdownModule } from 'ngx-markdown';
 
 import { ChatComponent  } from './chat/chat.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,10 @@ import { ChatComponent  } from './chat/chat.component';
     ])
   ],
   providers: [
-    provideHttpClient() // Provide HttpClient here
+    provideHttpClient(),
+    provideFirebaseApp(() => initializeApp({"projectId":"socraticbot-4bc8c","appId":"1:47141419868:web:6c6674a4653e59b60edf99","storageBucket":"socraticbot-4bc8c.appspot.com","apiKey":"AIzaSyAa6TUEJ0gc-YFQxQEciPBxfYtl9RnHWxg","authDomain":"socraticbot-4bc8c.firebaseapp.com","messagingSenderId":"47141419868"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()) // Provide HttpClient here
   ],
   bootstrap: [AppComponent]
 })
