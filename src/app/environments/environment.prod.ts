dc5c3cfab17daea2000391df66b279747d4c79dd
sam <samueljohnsiby@gmail.com>
2024-10-01 23:39:59 +0530

    config correction

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 export const environment = {
-  projectId: 'socraticbot-4bc8c',
-  appId: '1:47141419868:web:6c6674a4653e59b60edf99',
-  storageBucket: 'socraticbot-4bc8c.appspot.com',
-  apiKey: 'AIzaSyAa6TUEJ0gc-YFQxQEciPBxfYtl9RnHWxg',
-  authDomain: 'socraticbot-4bc8c.firebaseapp.com',
-  messagingSenderId: '47141419868',
-  apiUrl:''
-};
+    projectId: 'socraticbot-4bc8c',
+    appId: '1:47141419868:web:6c6674a4653e59b60edf99',
+    storageBucket: 'socraticbot-4bc8c.appspot.com',
+    apiKey: 'AIzaSyAa6TUEJ0gc-YFQxQEciPBxfYtl9RnHWxg',
+    authDomain: 'socraticbot-4bc8c.firebaseapp.com',
+    messagingSenderId: '47141419868',
+    apiUrl:'https://acharya-47141419868.asia-south1.run.app/',
+    apiPrivateKey:'kdjaoifKlke032__skIILlnkalle'
+  };
+  
