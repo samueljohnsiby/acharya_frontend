41ca35fb1a94f9ec761fb8b57c5bc2d1fe1ef704
sam <samueljohnsiby@gmail.com>
2024-10-02 00:18:35 +0530

    minor change

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment = {
   apiKey: 'AIzaSyAa6TUEJ0gc-YFQxQEciPBxfYtl9RnHWxg',
   authDomain: 'socraticbot-4bc8c.firebaseapp.com',
   messagingSenderId: '47141419868',
-  apiUrl:'https://acharya-47141419868.us-central1.run.app',
+  apiUrl:'https://acharya-47141419868.us-central1.run.app/',
   apiPrivateKey:'kdjaoifKlke032__skIILlnkalle'
 };
